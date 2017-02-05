8a2902d5c899743eba819382d783de6bba8f4276
n1cfury <fukugetin@gmail.com>
2017-02-04 16:00:18 -0800

    Zip File Password cracker p 25-29.  Initial commit.  More updates to come

---
+++ unzip.py
@@ -0,0 +1,37 @@
+#Violent Python p 25-29
+
+import zipfile, optparse
+from threading import Thread
+
+def banner():
+	print "##### Zip Cracker; p 25-29 ####"
+	print ""
+
+def extractFile(zFile, password):
+	try:
+		zFile.extractall(pwd=password)
+		print "[+] Found password"+password+"\n"
+	except:
+		pass
+
+def main():
+	banner()
+	parser = optparse.OptionParser("usage%prog "+"-f <zipfile> -d <dictionary>")
+	parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+	parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
+	(options, args) = parser.parse_args()
+	if (options.zname == None) |(options.dname == None):
+		print parser.usage
+		exit(0)
+	else:
+		zname = options.zname
+		dname = options.dname
+	zFile = zipfile.ZipFile(zname)
+	passFile = open(dname)
+	for line in passFile.readlines():
+		password = line.strip('\n')
+		t = Thread(target=extractFile, args=(zFile, password))
+		t.start()
+
+if __name__ == '__main__':
+	main()
