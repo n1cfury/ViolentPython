a77abacef9d03d42e0026fa2c31c15bd4c3ee718
n1cfury <fukugetin@gmail.com>
2017-02-05 18:16:29 -0800

    Updated comments

--- nmapScan.py
+++ nmapScan.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import nmap, optparse
 
 def banner():
@@ -27,14 +26,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-	parser = optparse.OptionParser('usage%prog '+'-H <target host> -p <target port>')
-	parser.add_option('-H', dest='tgtHost', type ='string', help='specify target host')
-	parser.add_option('-p', dest='tgtHost', type ='string', help ='specify target port[s] separated by comma')
-	(options, args) = parser.parse_args()
-	tgtHost = options.tgtHost
-	tgtPorts = str(options.tgtPort).split(', ')
-	if (tgtHost == None) | (tgtPorts[0] == None):
-		print parser.usage
-		exit(0)
-	for tgtPort in tgtPorts:
-		nmapScan(tgtHost, tgtPort)
