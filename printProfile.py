1469975200fdc2efe9ba88ecbe0224541af638f8
n1cfury <fukugetin@gmail.com>
2017-02-08 16:57:52 -0800

    Main, and Print Profile Functions complete.  Still in progress

--- printProfile.py
+++ printProfile.py
@@ -7,7 +7,16 @@ def banner():
 	print "__________-------------------_________"
 
 def printProfile(skypeDB):
-
+	conn = sqlite3.connect(skypeDB)
+	c = conn.cursor()
+	c.execute("SELECT fullname, skypename, city, country, datetime(profile_timestamp, 'unixepoch') FROM Accounts;")
+	for row in c:
+		print '[*] --Found Account--'
+		print '[+] User: '+str(row[0])
+		print '[+] Skype Username: '+str(row[1])
+		print '[+] Location: '+str(row[2])+','+str(row[3])
+		print '[+] Profile Date: '+str(row[4])
+		
 def printContacts(skypeDB):
 
 def printCallLog(skypeDB):
