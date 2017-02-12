92314959addc4ef636fff025c2b3e73a4031b1fa
n1cfury <fukugetin@gmail.com>
2017-02-11 19:23:42 -0800

    Created Empty functions.  finish it

--- uav-sniff.py
+++ uav-sniff.py
@@ -3,3 +3,22 @@
 
 def banner():
 	print "####   Drone Sniffer p195   #####"
+
+
+class interceptThread(threading, Thread):
+	def __init__(self):
+
+	def run(self):
+
+	def interceptPkt(self, pkt):
+
+	def injectCmd(self, cmd):
+
+	def emergencyland(self):
+
+	def takeoff(self):
+
+def main():
+
+if __name__ == '__main__':
+	main()
