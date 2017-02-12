870796d5a8871ca722f179ce11288b5064f89e49
n1cfury <fukugetin@gmail.com>
2017-02-11 19:18:59 -0800

    Created Empty functions.  Finish it

--- tcpHijack.py
+++ tcpHijack.py
@@ -3,3 +3,14 @@
 
 def banner():
 	print "####    TCP Hijack p 160    #####"
+
+def synFlood(src, tgt):
+
+def calTSN(tgt):
+
+def spoofConn(src, tgt, ack):
+
+def main():
+
+if __name__ == '__main__':
+	main()
