178695a794f2b11dc3154076d8b22ae646062cd8
n1cfury <fukugetin@gmail.com>
2017-02-11 19:12:32 -0800

    Set up creation of functions.  Needs to be finished

--- idsFoil.py
+++ idsFoil.py
@@ -1,5 +1,20 @@
-
+#!/usr/bin/env python
+import optparse
+from scapy.all import *
+from random import randint
 
 
 def banner():
 	print "####    Intrusion Detection Bypass p 166    #####"
+
+def ddosTest(src, dst, iface, count):
+
+def exploitTest(src, dst, iface, count):
+
+def scanTest(src, dst, iface, count):
+
+def main():
+
+if __name__ == '__main__':
+	main()
+
