7c6fa3b2a4245e565ed7f6c8d1b681afa28e9975
n1cfury <fukugetin@gmail.com>
2017-02-11 19:29:11 -0800

    Created empty functions

--- findDDoS.py
+++ findDDoS.py
@@ -2,3 +2,14 @@
 
 def banner():
 	print "####  ---Find DDoS p 142"
+
+def findDownload(pcap):
+
+def findHivemind(pcap):
+
+def findAttack(pcap):
+
+def main():
+
+if __name__ == '__main__':
+	main()
