c4e708ca9f43dec591cfe14f06d1ea9f4893c04f
n1cfury <fukugetin@gmail.com>
2017-02-14 21:01:57 -0800

    Created empty functions

---
+++ fireCatcher.py
@@ -0,0 +1,16 @@
+
+
+def banner():
+	print "###   Firesheep Detector p197-200   ###"
+	print ""
+
+def fireCatcher(pkt):
+
+def main():
+	banner()
+	fireCatcher(pkt):
+
+
+
+if __name__ == '__main__':
+	main()
