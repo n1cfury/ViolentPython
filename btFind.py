08331dd7c85017d792e9782f93857a93bd077a73
n1cfury <fukugetin@gmail.com>
2017-02-25 20:49:56 -0800

    This one was confusing.  This book gets confusing with some of the exercises.  Check your work

--- btFind.py
+++ btFind.py
@@ -1,11 +1,31 @@
-
+#!/usr/bin/env python
+import time
+from bluetooth import *
 
 def banner():
-	print "###   Bluetooth Detector p201  ###"
+	print "[***]	Bluetooth Detector p201	[***]"
+
+def lookup_name(device):
+	devList =discover_devices()
+	for device in devList:
+		name = str(lookup_name(device))
+		print "[+] Found Bluetooth Device "+str(name)
+		print "[+] MAC address: "+str(devices)
+		alreadyFound.append(addr)
+	while True:
+		findDevs()
+		time.sleep(5)
 
 def findDevs():
+	foundDevs = discover_devices(lookup_names = True)
+	for (addr,name)
+
+
+
+'''
 
 def main():
 
 if __name__ == '__main__':
 	main()
+'''
