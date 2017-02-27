493a51424d1f997200cefedb2c2b70fb476f71f2
n1cfury <fukugetin@gmail.com>
2017-02-26 22:07:52 -0800

    Code Completed.  DEBUG and TEST.

--- find-my-iphone.py
+++ find-my-iphone.py
@@ -1,11 +1,30 @@
-
+from scapy.all import *
+from bluetooth import *
 
 def banner():
-	print "   Find iphone bluetooth p203-206  ###"
+	print "[***]	Find iphone bluetooth p204	[***]"
 
-def retBtAddr():
+def retBtAddr(addr):
+	btAddr = str(hex(int(addr.replace(':', ''), 16)+1))[:2]
+	btAddr = btAddr[0:2]+":"+btAddr[4:6]+":"+\
+	btAddr[6:8]+":"+btAddr[8:10]+":"+btAddr[10:12]
+	return btAddr
 
-def main():
+def checkBluetooth(btAddr):
+	btName = lookup_name(btAddr)
+	if btName: 
+		print '[+] Detected Bluetooth Device: '+btName
+	else:
+		print '[-] Failed to Detect Bluetooth Device.'
 
-if __name__ == '__main__':
-	main()
+def wifiPrint(pkt):
+	iPhone_OUI = 'd0:23:db'
+	if pkt.haslayer(Dot11):
+		wifiMAC = kpt.getlayer(Dot11).addr2
+		if iPhone_OUI == wifiMAC[:8]:
+			print '[*] Detected iPhone MAC: '+wifiMAC
+			btAddr = retBtAddr(wifiMAC)
+			print '[+] Testing Bluetooth MAC: '+btAddr
+			checkBluetooth
+conf.iface = 'mon0'
+sniff(prn=wifiPrint)
