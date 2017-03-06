c49882745ad1fe714fb690e81e3950c92527a575
n1cfury <fukugetin@gmail.com>
2017-03-05 16:35:55 -0800

    code completed.  DEBUG and TEST

--- findDDoS.py
+++ findDDoS.py
@@ -9,7 +9,7 @@ def findDownload(pcap):
 		try:
 			eth = dpkt.ethernet.Ethernet(buf)
 			ip = eth.data
-			src = socket.inet_ntoa(ip, src)
+			src = socket.inet_ntoa(ip.src)
 			tcp = ip.data
 			http = dpkt.http.Request(tcp.data)
 			if http.method == 'GET':
@@ -24,7 +24,7 @@ def findHivemind(pcap):
 		try:
 			eth = dpkt.ethernet.Ethernet(buf)
 			ip = eth.data
-			src = socket.inet_ntoa(ip, src)
+			src = socket.inet_ntoa(ip.src)
 			tcp = ip.data
 			dport = tcp.dport
 			sport = tcp.sport
@@ -40,8 +40,45 @@ def findHivemind(pcap):
 			pass
 
 def findAttack(pcap):
+	pktCount = ()
+	for (ts, buf) in pcap:
+		try:
+			eth = dpkt.ethernet.Ethernet(buf)
+			ip = eth.data
+			src = socket.inet_ntoa(ip.src)
+			tcp = ip.data
+			dport = tcp.dport
+			if dport == 80:
+				stream = src+':'+dst
+				if pktCount.has_key(stream):
+					pktCount[stream] = pktCount[stream]+1
+				else:
+					pktCount[stream] = 1
+		except:
+			pass
+	for stream in pktCount:
+		pktsSent = pktCount[stream]
+		if pktsSent > THRESH:
+			src = stream.split(':')[0]
+			dst = stream.split(':')[1]
+			print '[+] '+src+' attacked '+dst+' with '+str(pktsSent)+' pkts.'
 
 def main():
+	parser = optparse.OptionParser('usage%prog '+'-p<pcap file> -t <thesh>>')
+	parser.add_option('-p', dest ='pcapFile', type='string', help ='specify pcap filename')
+	parser.add_option('-t', dest ='thresh', type='int', help ='specify threshold count')
+	(options, args) = parser.parse_args()
+	if options.pcapFile == None:
+		print parser.usage
+		exit(0)
+	if options.thresh != None:
+		THRESH = options.thresh
+	pcapFile = options.pcapFile
+	f = open(pcapFile)
+	pcap = dpkt.pcap.Reader(f)
+	findDownload(pcap)
+	findHivemind(pcap)
+	findAttack(pcap)
 
 if __name__ == '__main__':
 	main()
