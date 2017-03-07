c6d3df052fcb2167e429db1f4b41ef706d90d583
n1cfury <fukugetin@gmail.com>
2017-03-06 19:09:37 -0800

    Code Completed.  DEBUG and TEST

--- imageMirror.py
+++ imageMirror.py
@@ -1,11 +1,43 @@
-
+#!/usr/bin/env python
+from anonBrowser import *
+from BeautifulSoup import BeautifulSoup
+import os, optparse
 
 def banner():
-	print "### mirroring images p222 ###"
+	print "[***]	Mirroring Images (w/ Beautiful Soup) p222	[***]"
 
-def mirrorImages():
+def mirrorImages(url, dir):
+	ab = anonBrowser()
+	ab.anonymize()
+	html = ab.open(url)
+	soup = BeautifulSoup(html)
+	image_tags = soup.findall('img')
+	for image in image_tags:
+		filename = image['src'].lstring('http://')
+		filename = os.path.join(dir,filename.replace('/','_'))
+		print '[+] Saving '+str(filename)
+		data = ab.open(image['src']).read()
+		ab.back()
+		save=open(filename, 'wb')
+		save.write(data)
+		save.close()
 
 def main():
+	parser = optparse.OptionParser('usage%prog '+'-u <target url> -d <destination directory>')
+	parser.add_option('-u', dest='tgtURL', type ='string', help='specify target URL')
+	parser.add_option('-d', dest='dir', type ='string', help='specify destination directory')
+	(options, args) = parser.parse_args()
+	url = option.tgtURL
+	dir = options.dir
+	if url == None or dir == None:
+		print parser.usage
+		exit(0)
+	else:
+		try:
+			mirrorImages(url, dir)
+		except Exception, e:
+			print '[-] Error Mirroring Images.'
+			print '[-] '+str(e)
 
 if __name__ == '__main__':
 	main()
