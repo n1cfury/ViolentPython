5a5e4f24a338e57929d84c921680a99d732478e3
n1cfury <fukugetin@gmail.com>
2017-03-07 20:59:44 -0800

    Code Completed.  DEBUG and TEST

--- rfcommScan.py
+++ rfcommScan.py
@@ -1,11 +1,20 @@
-
+from bluetooth import *
 
 def banner():
-	print "   RFCOMM scan p 205-206  ###"
+	print "[***]	RFCOMM scan p 206	[***]"
 
-def rfcommCon():
+def rfcommCon(addr, port):
+	sock = BluetoothSocket(RFCOMM)
+	try:
+		sock.connect((addr, port))
+		print '[+] RFCOMM Port '+str(port)+' open'
+		sock.close()
+	except Exception, e:
+		print '[-] RFCOMM Port '+str(port)+ ' closed'
 
 def main():
+	for port in range(1,30):
+	rfcommCon('00:16:38:DE:AD:11', port)
 
 if __name__ == '__main__':
 	main()
