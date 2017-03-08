132f5beb442f9c9605ed2c559030868e9962bb23
n1cfury <fukugetin@gmail.com>
2017-03-07 20:59:14 -0800

    Code Completed.  DEBUG and TEST

--- proxyTest.py
+++ proxyTest.py
@@ -1,12 +1,20 @@
-
+import mechanize
+url = 'http://ip.nefsc.noaa.gov'
+hideMeProxy = {'http':'216.155.139.115:3128'}
 
 def banner():
-	print "### Proxy p214 ###"
+	print "[***]	Anon Proxy p214		[***]"
 
 def testProxy(url, proxy):
-
+	browser = mechanize.Browser()
+	browser.set_proxies(proxy)
+	page = browser.open(url)
+	source_code = page.read()
+	print source_code
 
 def main():
+	banner()
+	testProxy(url,hideMeProxy)
 
 if __name__ == '__main__':
 	main()
