60f512804b4ad60379510126b303912b01f190c7
n1cfury <fukugetin@gmail.com>
2017-03-09 16:59:26 -0800

    Code Completed.  DEBUG and TEST

--- sdpScan.py
+++ sdpScan.py
@@ -1,11 +1,19 @@
-
+from bluetooth import *
 
 def banner():
-	print "   Bluetooth SDP scan p207 ###"
+	print "[***]	Bluetooth SDP scan p207		[***]"
 
 def sdpBrowse(addr):
+	services = find_service(address=addr)
+	for service in services:
+		name = service['name']
+		proto = service['protocol']
+		port = str(service['port'])
+		print '[+] Found '+str(name)+' on '+str(proto)+':'+port
 
 def main():
+	banner()
+	sdpBrowse(addr)		#addr = MAC address
 
 if __name__ == '__main__':
 	main()
