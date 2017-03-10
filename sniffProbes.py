f55ebb988e6d3be9fd03264d8d3ee56ebdc569e0
n1cfury <fukugetin@gmail.com>
2017-03-09 17:13:14 -0800

    Code Completed.  DEBUG and TEST

--- sniffProbes.py
+++ sniffProbes.py
@@ -1,9 +1,20 @@
-
-
+from scapy.all import *
+interface = 'mon0'
+probeReqs = []
 
 def banner():
-	print "####   802.11 Probe Requests p187   #####"
+	print "[***]	802.11 Probe Requests p187	[***]"
 
+def sniffProbe(p):
+	if p.haslayer(Dot11ProbeReq):
+		netName = p.getlayer(Dot11ProbeReq).info
+		if netName not in probeReqs:
+			probeReqs.append(netName)
+			print '[+] Detected New Probe Request: '+netName
 
-def sniffDot11():
+def main():
+	def banner()
+	sniff(iface=interface, prn=sniffProbe)
 
+if __name__ == '__main__':
+	main()
