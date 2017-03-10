841565d29e7bfa4d474f8771579f367597f386a1
n1cfury <fukugetin@gmail.com>
2017-03-09 16:53:19 -0800

    Code Completed.  DEBUG and TEST

--- sendSpam.py
+++ sendSpam.py
@@ -1,12 +1,35 @@
+import smtplib
 
+user = 'username'
+pwd = 'password'
+msg =MIMEText(text)
+msg['From'] = user
+msg['To'] = to
+msg['subject'] = subject
 
 def banner():
-	print "### sned spam p238 ###"
+	print "[***]	Anon-mail p238		[***]"
 
-def sendMail():
-
+def sendMail(user, pwd, to, subject, text):
+	try:
+		smtpServer = smtplib.SMTP('smtp.gmail.com', 587)
+		print "[+] Connecting to Mail Server."
+		smtpServer.ehlo()
+		print "[+] Starting Encrypted Session."
+		smtpServer.starttls()
+		smtpServer.ehlo()
+		print "[+] Logging Into Mail Server."
+		smtpServer.login(user, pwd)
+		print "[+] Sending Mail."
+		smtpServer.sendmail(user, to, msg.as_string())
+		smtpServer.close()
+		print "[+] Mail Sent Successfully."
+	except:
+		print "[-] Sending Mail failed."
 
 def main():
+	banner()
+	sendMail(user, pwd, '<target email address>', 'Re: Important', 'Test Message')
 
 if __name__ == '__main__':
 	main()
