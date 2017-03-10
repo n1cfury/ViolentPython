b8860c795914ed4fae80603964e7e34a83cc647a
n1cfury <fukugetin@gmail.com>
2017-03-09 18:10:29 -0800

    Code Completed.  DEBUG and TEST

--- testFastFlux.py
+++ testFastFlux.py
@@ -1,12 +1,24 @@
-
-
+from scapy.all import *
+dnsRecords = {}
 
 def banner():
-	print "####    Test Fast Flux Traffic p152    #####"
+	print "[***]	Detect Fast Flux p152		[***]"
 
 def handlePkt(pkt):
+	if pkt.haslayer(DNSRR):
+		rrname = pkt.getlayer(DNSRR).rrname
+		rdata = pkt.getlayer(DNSRR).rdata
+		if dnsRecords.has_key(rrname):
+			if radata not in dnsRecords[rrname]:
+				dnsRecords[rrname].append(rdata)
 
 def main():
+	banner()
+	pkts = rdpcap('fastFlux.pcap')
+	for pkt in pkts:
+		handlePkt(pkt)
+	for item in dnsRecords:
+		print '[+] '+item+' has '+str(len(dnsRecords[item]))+' unique IPs.'
 
 if __name__ == '__main__':
 	main()
