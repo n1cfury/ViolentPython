832d4f18c50d695bb9dbd779e64ed45721f64976
n1cfury <fukugetin@gmail.com>
2017-03-22 21:54:42 -0700

    Code Completed.  DEBUG and TEST

--- viewPage.py
+++ viewPage.py
@@ -1,12 +1,17 @@
-
+import mechanize
 
 def banner():
 	print "[***]	Page Viewer p213	[***]"
 
 def viewPage(url):
-
+	browser = mechanize.Browser()
+	page = browser.open(url)
+	source_code=page.read()
+	print source_code
 
 def main():
+	banner()
+	viewPage('http://www.syngress.com')
 
 if __name__ == '__main__':
 	main()
